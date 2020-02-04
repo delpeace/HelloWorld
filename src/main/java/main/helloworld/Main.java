93a14f8687b9170f06fa99d4b1a2857462e9d4f1
Milica Živanović <milica.zivanovic6@gmail.com>
2020-02-04 20:19:45 +0100

    Adds age checker

--- src/main/java/main/helloworld/Main.java
+++ src/main/java/main/helloworld/Main.java
@@ -1,5 +1,7 @@
 package main.helloworld;
 
+import java.util.Scanner;
+
 /**
  *
  * @author qa
@@ -13,6 +15,51 @@ public class Main {
         
         System.out.println("Hello World!");
         System.out.println("Milica Zivanovic");
+        
+        int age = 5;
+        
+        if (age > 18) {
+            System.out.println("Osoba je punoletna.");
+        } else {
+            System.out.println("Osoba je maloletna.");
+        }
+        
+        System.out.println("Unesite broj godina: ");
+        Scanner input = new Scanner(System.in);
+        int userAge = input.nextInt();
+        
+        System.out.println("Korisnik je uneo: " + userAge);
+        
+        if (userAge >= 18) {
+            System.out.println("Osoba je punoletna.");
+        } else {
+            System.out.println("Osoba je maloletna.");
+        }
+    
+
+       
+    
+        System.out.println("Unesite broj godina: ");
+        Scanner input2 = new Scanner(System.in);
+        int personAge = input.nextInt();
+        
+        
+        if (personAge <= 0 && personAge > 150) {
+            System.out.println("Invalid");
+        } else if (personAge > 0 && personAge <= 5) {
+            System.out.println("Baby");
+        } else if (personAge <= 11) {
+            System.out.println("Kid");
+        } else if (personAge <= 17) {
+            System.out.println("Teen");
+        }  else if (personAge >=18) {
+            System.out.println("Adult");
+        }
+        
+            
+        
+        
+        
     }
     
 }
